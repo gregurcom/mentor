a87e6a41c3b291f6f69d85403dc5fd28051d0761
mihai <botanmazuro@gmail.com>
2021-09-26 00:44:29 +0300

    add strict types, add phpdocs

--- app/Models/CourseUser.php
+++ app/Models/CourseUser.php
@@ -7,6 +7,24 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Models\CourseUser
+ *
+ * @property int $id
+ * @property int $user_id
+ * @property int $course_id
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @method static \Illuminate\Database\Eloquent\Builder|CourseUser newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|CourseUser newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|CourseUser query()
+ * @method static \Illuminate\Database\Eloquent\Builder|CourseUser whereCourseId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|CourseUser whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|CourseUser whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|CourseUser whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|CourseUser whereUserId($value)
+ * @mixin \Eloquent
+ */
 class CourseUser extends Model
 {
     use HasFactory;
--- app/Models/Rate.php
+++ app/Models/Rate.php
@@ -8,6 +8,28 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
+/**
+ * App\Models\Rate
+ *
+ * @property int $id
+ * @property int $rate
+ * @property int $user_id
+ * @property int $course_id
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \App\Models\Course $course
+ * @method static \Database\Factories\RateFactory factory(...$parameters)
+ * @method static \Illuminate\Database\Eloquent\Builder|Rate newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Rate newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Rate query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Rate whereCourseId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Rate whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Rate whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Rate whereRate($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Rate whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Rate whereUserId($value)
+ * @mixin \Eloquent
+ */
 class Rate extends Model
 {
     use HasFactory;

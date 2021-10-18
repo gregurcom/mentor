5570615191d25ff008f26b6e7f5d107c53a80eb9
mihai <botanmazuro@gmail.com>
2021-10-18 11:19:49 +0300

    change og image

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -6,7 +6,7 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta property="og:site_name" content="{{ config('app.name') }}" />
-    <meta property="og:image" content="{{ asset('images/favicon.ico') }}" />
+    <meta property="og:image" content="{{ asset('images/ogImage.png') }}" />
     <meta property="og:title" content="@yield('title', 'Mentor')" />
     @yield('meta')
 

004991491447ad0e861ec846c28a6b3f822d24b3
mihai <botanmazuro@gmail.com>
2021-10-25 14:42:19 +0300

    write http tests, modify sub link

--- tests/Feature/CourseTest.php
+++ tests/Feature/CourseTest.php
@@ -4,16 +4,23 @@ declare(strict_types = 1);
 
 namespace Tests\Feature;
 
+use App\Models\Category;
+use App\Models\Course;
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class CourseTest extends TestCase
 {
-    public function test_example(): void
+    public function test_course(): void
     {
-        $response = $this->get('/courses');
+        $user = User::factory()->create();
+        $category = Category::factory()->create();
+        $course = Course::factory()->create(['user_id' => $user->id, 'category_id' => $category->id]);
 
-        $response->assertStatus(200);
+        $response = $this->get('courses/' . $course->slug);
+
+        $response->assertStatus(200)->assertSee($course->title);
     }
 }
---
+++ tests/Feature/DashboardTest.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Tests\Feature;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class DashboardTest extends TestCase
+{
+    public function test_visit_dashboard_unauthorized(): void
+    {
+        $response = $this->get('dashboard');
+
+        $response->assertStatus(302);
+    }
+
+    public function test_visit_dashboard_authorized(): void
+    {
+        $user = User::factory()->create();
+
+        $response = $this->actingAs($user)->get('dashboard');
+
+        $response->assertStatus(200);
+    }
+}

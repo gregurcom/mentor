4c7d026ae95d91b1cf86e95f383c7dd5758fef5d
mihai <botanmazuro@gmail.com>
2021-10-31 00:11:39 +0300

    modify user table, rename tables so that voyager migrations go after

---
+++ database/migrations/2015_10_30_210022_modify_users_table.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types = 1);
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class ModifyUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('remember_token')->nullable();
+            $table->timestamp('email_verified_at')->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('remember_token');
+            $table->timestamp('email_verified_at')->change();
+        });
+    }
+}

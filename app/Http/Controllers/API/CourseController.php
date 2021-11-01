1ba786144c19364b201acfd66ebed4c968348479
mihai <botanmazuro@gmail.com>
2021-11-01 15:59:48 +0200

    create api auth with sanctum

--- app/Http/Controllers/API/CourseController.php
+++ app/Http/Controllers/API/CourseController.php
@@ -5,9 +5,12 @@ declare(strict_types = 1);
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\CourseRequest;
 use App\Http\Resources\CourseResource;
 use App\Models\Course;
 use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Auth;
 
 class CourseController extends Controller
 {
@@ -19,5 +22,26 @@ class CourseController extends Controller
     public function show(Course $course): CourseResource
     {
         return new CourseResource($course);
+    }
+
+    public function store(CourseRequest $request): Response
+    {
+        $course = Course::create(array_merge(['user_id' => Auth::id()], $request->validated()));
+
+        return response(['course' => $course], 201);
+    }
+
+    public function update(Course $course, CourseRequest $request): response
+    {
+        $course->update($request->validated());
+
+        return response(['course' => $course], 204);
+    }
+
+    public function destroy(Course $course): Response
+    {
+        $course->delete();
+
+        return response(['message' => 'Destroy course'], 200);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 declare(strict_types = 1);
 
+use App\Http\Controllers\API\AuthController;
 use App\Http\Controllers\API\CourseController;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +17,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::apiResource('courses', CourseController::class)->only(['index', 'show']);
+Route::post('auth/register', [AuthController::class, 'register'])->name('register');
+Route::post('auth/login', [AuthController::class, 'login'])->name('login');
+
+Route::get('courses', [CourseController::class, 'index'])->name('courses.index');
+Route::get('courses/{course}', [CourseController::class, 'show'])->name('courses.show');
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('courses', [CourseController::class, 'store'])->name('courses.store');
+    Route::put('courses/{course}', [CourseController::class, 'update'])->name('courses.update');
+    Route::delete('courses/{course}', [CourseController::class, 'destroy'])->name('courses.destroy');
+
+    Route::post('auth/logout', [AuthController::class, 'logout'])->name('logout');
+});

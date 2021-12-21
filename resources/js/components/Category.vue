e43f8b4f26362b961f48ecc508e22bf8322c8efa
mihai <botanmazuro@gmail.com>
2021-12-21 17:23:04 +0200

    modify category page loading

--- resources/js/components/Category.vue
+++ resources/js/components/Category.vue
@@ -1,13 +1,27 @@
 <template>
-    <div :class="{'loading': loading}">
+    <div v-if="loading === false">
+        <div class="text-center">
+            <img :src="'/images/smile.png'" width="600" height="300" id="course-image">
+        </div>
         <span v-for="category in categories" class="px-2">
             <a :href="`/categories/${category.id}/courses`" class="btn btn-outline-dark mt-2">{{ category.name }}</a>
         </span>
     </div>
+    <div v-else class="atom">
+        <atom-spinner
+            :animation-duration="1000"
+            :size="80"
+            :color="'#69d2f1'"
+        />
+    </div>
 </template>
 
 <script>
+    import {AtomSpinner} from 'epic-spinners'
     export default {
+        components: {
+            AtomSpinner
+        },
         data() {
             return {
                 categories: [],
--- resources/views/platform/categories.blade.php
+++ resources/views/platform/categories.blade.php
@@ -4,9 +4,6 @@
 
 @section('content')
     <div id="app" class="container wrapper flex-grow-1 mt-3 mb-3">
-        <div class="text-center">
-            <img src="{{ asset('images/smile.png') }}" width="600" height="300" id="course-image">
-        </div>
         <category-page></category-page>
     </div>
 @endsection

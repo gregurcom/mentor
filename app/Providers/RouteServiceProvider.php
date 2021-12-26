d3f1a7f371506c0efd92d832ef1f75391e820739
mihai <botanmazuro@gmail.com>
2021-12-26 09:34:48 +0200

    restructure routes

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -49,6 +49,11 @@ class RouteServiceProvider extends ServiceProvider
             Route::middleware('web')
                 ->namespace($this->namespace)
                 ->group(base_path('routes/web.php'));
+
+            Route::middleware('web')
+                ->namespace($this->namespace)
+                ->name('platform.')
+                ->group(base_path('routes/platform.php'));
         });
 
         Route::pattern('id', '[0-9]+');
--- routes/web.php
+++ routes/web.php
@@ -7,13 +7,6 @@ use App\Http\Controllers\Auth\PasswordResetController;
 use App\Http\Controllers\Platform\LocaleController;
 use App\Http\Controllers\Auth\AccessController;
 use App\Http\Controllers\Auth\RegistrationController;
-use App\Http\Controllers\Platform\CourseController;
-use App\Http\Controllers\Platform\FileController;
-use App\Http\Controllers\Platform\LessonController;
-use App\Http\Controllers\Platform\RateController;
-use App\Http\Controllers\Platform\SubscriptionController;
-use App\Http\Controllers\Platform\TaskController;
-use App\Http\Controllers\Platform\TechSupportController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -41,56 +34,6 @@ Route::name('auth.')->group(function () {
         Route::post('registration', [RegistrationController::class, 'save'])->name('registration.save');
     });
     Route::get('logout', [AccessController::class, 'logout'])->name('logout');
-});
-
-Route::name('platform.')->group(function () {
-    Route::get('categories/{category}/courses', [CourseController::class, 'list'])->name('courses.list');
-
-    Route::view('feed', 'platform.feed')->name('feed.index');
-
-    Route::name('subscriptions.')->group(function () {
-        Route::post('subscriptions/{course}', [SubscriptionController::class, 'store'])->name('store');
-        Route::delete('subscriptions/{course}', [SubscriptionController::class, 'destroy'])->name('destroy');
-    });
-
-    Route::view('categories', 'platform.categories')->name('categories.list');
-    Route::view('subscriptions', 'platform.subscriptions')->name('subscriptions.index');
-
-    Route::get('courses/{course}', [CourseController::class, 'show'])->name('courses.show');
-
-    Route::resource('lessons', LessonController::class)->except(['index', 'create']);
-    Route::get('lessons/create/{course}', [LessonController::class, 'create'])->name('lessons.create');
-
-    Route::get('search', [CourseController::class, 'search'])->name('course.search');
-
-    Route::middleware(['auth', 'verified'])->group(function () {
-        Route::get('file/{file}/download', FileController::class)->name('file.download');;
-
-        Route::get('courses/rate/{course}', RateController::class)->name('course.rate');
-
-        Route::get('tech-support', [TechSupportController::class, 'show'])->name('tech-support');
-        Route::post('tech-support', [TechSupportController::class, 'send'])
-            ->name('tech-support.send')
-            ->middleware('throttle:tech-support');
-
-        Route::view('tasks', 'platform.task.index')->name('tasks');
-        Route::get('tasks/{task}/edit', [TaskController::class, 'edit'])->name('tasks.edit')
-            ->can('edit', 'task');
-        Route::put('tasks/{task}/update', [TaskController::class, 'update'])->name('tasks.update')
-            ->can('update', 'task');
-
-        Route::get('courses/create', [CourseController::class, 'create'])->name('courses.create');
-        Route::post('courses', [CourseController::class, 'store'])->name('courses.store');
-        Route::get('courses/{course}/edit', [CourseController::class, 'edit'])
-            ->name('courses.edit')
-            ->can('edit', 'course');
-        Route::put('courses/{course}', [CourseController::class, 'update'])
-            ->name('courses.update')
-            ->can('update', 'course');
-        Route::delete('courses/{course}', [CourseController::class, 'destroy'])
-            ->name('courses.destroy')
-            ->can('destroy', 'course');
-    });
 });
 
 Route::name('verification.')->group(function () {

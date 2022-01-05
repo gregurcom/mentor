bb055b9da1b59c06225724f03f8d31aadf8c9b02
mihai <botanmazuro@gmail.com>
2022-01-05 14:36:59 +0200

    responsive settings view

--- resources/views/auth/settings.blade.php
+++ resources/views/auth/settings.blade.php
@@ -4,14 +4,14 @@
 
 @section('content')
     <div class="container">
-        <div class="row">
+        <div class="row mt-3 mb-3">
             @if (session('status'))
                 <div class="alert alert-dark mt-2 text-center">
                     {{ session('status') }}
                 </div>
             @endif
             <div class="col-md-6 text-center">
-                <img src="{{ asset('images/' . Auth::user()->avatar) }}" width="150" height="200" class="mb-2">
+                <img src="{{ asset('images/' . Auth::user()->avatar) }}" width="150" height="200" class="mb-3">
                 @error('image')
                     <div class="alert alert-danger mt-1">
                         {{ $message }}
@@ -25,30 +25,33 @@
                 </form>
             </div>
             <div class="col-md-6">
-                <form action="{{ route('auth.settings.modify-name') }}" method="POST">
-                    @csrf
+                <div class="settings-data">
+                    <form action="{{ route('auth.settings.modify-name') }}" method="POST">
+                        @csrf
 
-                    <input type="text" name="name" value="{{ Auth::user()->name }}" class="form-control w-25 d-inline">
-                    <button type="submit" class="btn btn-outline-dark">Modify</button>
-                </form>
-                <form action="{{ route('auth.settings.modify-password') }}" method="POST" class="mt-2">
-                    @csrf
+                        <input type="text" name="name" value="{{ Auth::user()->name }}" class="form-control w-25 d-inline">
+                        <button type="submit" class="btn btn-outline-dark">Modify</button>
+                    </form>
+                    <form action="{{ route('auth.settings.modify-password') }}" method="POST" class="mt-2">
+                        @csrf
 
-                    <input type="password" name="old_password" class="form-control mt-1 w-50" placeholder="Old password...">
-                    @error('old_password')
-                        <div class="alert alert-danger mt-1 w-50">
-                            {{ $message }}
-                        </div>
-                    @enderror
-                    <input type="password" name="password" class="form-control mt-1 w-50" placeholder="New password...">
-                    @error('password')
-                        <div class="alert alert-danger mt-1 w-50">
-                            {{ $message }}
-                        </div>
-                    @enderror
-                    <input type="password" name="password_confirmation" class="form-control mt-1 w-50" placeholder="Conform new password...">
-                    <button type="submit" class="btn btn-outline-dark mt-2">Modify</button>
-                </form>
+                        <input type="password" name="old_password" class="form-control mt-1 w-50 d-inline" placeholder="Old password...">
+                        @error('old_password')
+                            <div class="alert alert-danger mt-1 w-50">
+                                {{ $message }}
+                            </div>
+                        @enderror
+                        <input type="password" name="password" class="form-control mt-1 w-50 d-inline" placeholder="New password...">
+                        @error('password')
+                            <div class="alert alert-danger mt-1 w-50">
+                                {{ $message }}
+                            </div>
+                        @enderror
+                        <input type="password" name="password_confirmation" class="form-control mt-1 w-50 d-inline" placeholder="Conform new password...">
+                        <br>
+                        <button type="submit" class="btn btn-outline-dark mt-2">Modify</button>
+                    </form>
+                </div>
             </div>
             <div class="col-md-6">
             </div>
